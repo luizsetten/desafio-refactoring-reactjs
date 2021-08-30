cd684e1053c622a44f800a41e4d2390882ac4bf3
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-08-29 22:30:55 -0300

    refact: input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,12 +5,19 @@ import {
   useCallback,
 } from 'react';
 
+import { IconType } from 'react-icons'
+
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type InputProps = React.HTMLProps<HTMLInputElement> & {
+  name: string;
+  icon: IconType;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
